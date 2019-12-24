deaea44b9a903666e38f7d2d1da259053bbbb506
SpursLipu <844701730@qq.com>
2019-12-24 20:05:10 +0800

    first commit

---
+++ convert.py
@@ -0,0 +1,70 @@
+import argparse
+from sys import platform
+
+from models import *  # set ONNX_EXPORT in models.py
+from utils.datasets import *
+from utils.utils import *
+
+
+def convert():
+    img_size = (320, 192) if ONNX_EXPORT else opt.img_size  # (320, 192) or (416, 256) or (608, 352) for (height, width)
+    weights, half, view_img = opt.weights, opt.half, opt.view_img
+
+    # Initialize
+    device = torch_utils.select_device(device='cpu' if ONNX_EXPORT else opt.device)
+
+    # Initialize model
+    model = Darknet(opt.cfg, img_size)
+
+    # Load weights
+    attempt_download(weights)
+    if weights.endswith('.pt'):  # pytorch format
+        model.load_state_dict(torch.load(weights, map_location=device)['model'])
+    else:  # darknet format
+        _ = load_darknet_weights(model, weights)
+
+    # Second-stage classifier
+    classify = False
+    if classify:
+        modelc = torch_utils.load_classifier(name='resnet101', n=2)  # initialize
+        modelc.load_state_dict(torch.load('weights/resnet101.pt', map_location=device)['model'])  # load weights
+        modelc.to(device).eval()
+
+    # Fuse Conv2d + BatchNorm2d layers
+    # model.fuse()
+
+    # Eval mode
+    model.to(device).eval()
+
+    # Export mode
+    if ONNX_EXPORT:
+        img = torch.zeros((1, 3) + img_size)  # (1, 3, 320, 192)
+        torch.onnx.export(model, img, 'weights/export.onnx', verbose=True)
+        return
+
+    # Half precision
+    half = half and device.type != 'cpu'  # half precision only supported on CUDA
+    if half:
+        model.half()
+    save_weights(model, path='weights/Dior-tiny-best.weights')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--cfg', type=str, default='cfg/yolov3-spp.cfg', help='cfg file path')
+    parser.add_argument('--data', type=str, default='data/coco.data', help='coco.data file path')
+    parser.add_argument('--weights', type=str, default='weights/yolov3-spp.weights', help='path to weights file')
+    parser.add_argument('--source', type=str, default='data/samples', help='source')  # input file/folder, 0 for webcam
+    parser.add_argument('--output', type=str, default='output', help='output folder')  # output folder
+    parser.add_argument('--img-size', type=int, default=416, help='inference size (pixels)')
+    parser.add_argument('--conf-thres', type=float, default=0.6, help='object confidence threshold')
+    parser.add_argument('--nms-thres', type=float, default=0.8, help='iou threshold for non-maximum suppression')
+    parser.add_argument('--fourcc', type=str, default='mp4v', help='output video codec (verify ffmpeg support)')
+    parser.add_argument('--half', action='store_true', help='half precision FP16 inference')
+    parser.add_argument('--device', default='', help='device id (i.e. 0 or 0,1) or cpu')
+    parser.add_argument('--view-img', action='store_true', help='display results')
+    opt = parser.parse_args()
+    print(opt)
+
+    with torch.no_grad():
+        convert()
---
+++ utils/quantized.py
@@ -0,0 +1,154 @@
+# Author:LiPu
+# Author:LiPu
+import math
+import torch
+import torch.nn as nn
+from torch.autograd import Function
+import torch.nn.functional as F
+
+
+# 定义前向传播，反向传播三值化函数
+class Ternarize(Function):
+    '''
+    Binarize the input activations and calculate the mean across channel dimension.
+    '''
+
+    # 使用静态方法定义三值激活类
+    @staticmethod
+    def forward(self, input):
+        self.save_for_backward(input)
+        output = input.new(input.size())
+        output[input > 0.5] = 1
+
+        # 由于暂时不知道pytorch如何进行与运算，用此段代码实现
+        # output[input>=-0.5 and input<=0.5]
+        temp = torch.add((input >= -0.5), (input <= 0.5))
+        temp[temp == 2] = 1
+        temp[temp == 1] = 0
+        output[temp] = 0
+
+        output[input < -0.5] = -1
+        return output
+
+    @staticmethod
+    def backward(self, grad_output):
+        input, = self.saved_tensors
+        grad_input = grad_output.clone()
+        # grad_input[input.ge(1)] = 0
+        # grad_input[input.le(-1)] = 0
+        return grad_input
+
+
+# 定义前向传播，反向传播二值化函数
+class Binarize(Function):
+    @staticmethod
+    def forward(self, input):
+        self.save_for_backward(input)
+        output = input.new(input.size())
+        output[input >= 0] = 1
+        output[input < 0] = 0
+        return output
+
+    @staticmethod
+    def backward(cxt, grad_output):
+        input = cxt.saved_tensors
+        grad_input = grad_output.clone()
+        # grad_input[input < -1] = 0
+        # grad_input[input >= 1] = 0
+        return grad_input
+
+
+# 定义前向传播，反向传播权值归一化函数
+class WBN(Function):
+    @staticmethod
+    def forward(self, input):
+        self.save_for_backward(input)
+        output = (input - torch.mean(input)) / torch.sqrt(torch.std(input))
+        return output
+
+    @staticmethod
+    def backward(cxt, grad_output):
+        input = cxt.saved_tensors
+        grad_input = grad_output.clone()
+        # grad_input[input < -1] = 0
+        # grad_input[input >= 1] = 0
+        return grad_input
+
+
+# 定义前向传播，反向传播八位定点量化函数
+class EQ(Function):
+    @staticmethod
+    def forward(self, input):
+        self.save_for_backward(input)
+        n = float(2 ** 8 - 1)
+        out = torch.round(input * n) / n
+        return out
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        grad_input = grad_output.clone()
+        return grad_input
+
+
+binarize = Binarize.apply
+
+ternarize = Ternarize.apply
+
+WBN = WBN.apply
+
+EQ = EQ.apply
+
+
+# 重载LeakyRelu
+class BinaryLeakyReLU(nn.LeakyReLU):
+    def __init__(self):
+        super(BinaryLeakyReLU, self).__init__()
+
+    def forward(self, input):
+        output = EQ(input)
+        return output
+
+
+# 对线性层权重做量化，必须有reset_parameters函数
+class BinaryLinear(nn.Linear):
+
+    def forward(self, input):
+        binary_weight = ternarize(self.weight)
+        if self.bias is None:
+            return F.linear(input, binary_weight)
+        else:
+            return F.linear(input, binary_weight, self.bias)
+
+    def reset_parameters(self):
+        # Glorot initialization
+        in_features, out_features = self.weight.size()
+        stdv = math.sqrt(1.5 / (in_features + out_features))
+        self.weight.data.uniform_(-stdv, stdv)
+        if self.bias is not None:
+            self.bias.data.zero_()
+
+        self.weight.lr_scale = 1. / stdv
+
+
+# 对卷积层权重做量化
+class BinaryConv2d(nn.Conv2d):
+
+    def forward(self, input):
+        bw = WBN(self.weight)
+        bw = ternarize(bw)
+        return F.conv2d(input, bw, self.bias, self.stride,
+                        self.padding, self.dilation, self.groups)
+
+    def reset_parameters(self):
+        # Glorot initialization
+        in_features = self.in_channels
+        out_features = self.out_channels
+        for k in self.kernel_size:
+            in_features *= k
+            out_features *= k
+        stdv = math.sqrt(1.5 / (in_features + out_features))
+        self.weight.data.uniform_(-stdv, stdv)
+        if self.bias is not None:
+            self.bias.data.zero_()
+
+        self.weight.lr_scale = 1. / stdv

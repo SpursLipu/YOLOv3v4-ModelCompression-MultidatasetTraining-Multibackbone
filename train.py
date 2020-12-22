f093ec9cac848f5b1d1d78caf912a6cba5b3a082
SpursLipu <844701730@qq.com>
2020-12-22 19:15:54 +0800

    add augmentation method

--- train.py
+++ train.py
@@ -126,9 +126,10 @@ def train(hyp):
     # seed = int(img_size / 32)
     # gridmask = GridMask(d1=96, d2=224, rotate=360, ratio=0.6, mode=1, prob=0.8)
 
-    # print('<.....................using fencemask.......................>')
-    # fencemask = FenceMask(seed, seed * 3, seed * 4, seed * 8, [0, 0, 0], 0.8)
-    # max_epoch = int(epochs * 0.8)
+    print('<.....................using fencemask.......................>')
+    seed = int(img_size / 32)
+    fencemask = FenceMask(seed, seed * 3, seed * 4, seed * 8, [0, 0, 0], 0.8)
+    max_epoch = int(epochs * 0.8)
     start_epoch = 0
     best_fitness = 0.0
     if weights != 'None':
@@ -284,7 +285,7 @@ def train(hyp):
     print('Using %g dataloader workers' % nw)
     print('Starting training for %g epochs...' % epochs)
     for epoch in range(start_epoch, epochs):  # epoch ------------------------------------------------------------------
-        # fencemask.set_prob(epoch, max_epoch)
+        fencemask.set_prob(epoch, max_epoch)
         # gridmask.set_prob(epoch, max_epoch)
         model.train()
         # 稀疏化标志
@@ -323,7 +324,7 @@ def train(hyp):
                     ns = [math.ceil(x * sf / gs) * gs for x in imgs.shape[2:]]  # new shape (stretched to 32-multiple)
                     imgs = F.interpolate(imgs, size=ns, mode='bilinear', align_corners=False)
             # Forward
-            # imgs = fencemask(imgs)
+            imgs = fencemask(imgs)
             # imgs = gridmask(imgs)
             targets = targets.to(device)
             pred, feature_s = model(imgs)

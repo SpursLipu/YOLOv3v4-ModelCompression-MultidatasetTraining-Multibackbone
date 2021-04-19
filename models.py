1d17e1c1011ab2982dfb4b142b59429361e1c40f
BillAmihom <yhy@stu.pku.edu.cn>
2021-04-19 14:20:16 +0800

    quantizer_output reorder

--- models.py
+++ models.py
@@ -12,7 +12,7 @@ ONNX_EXPORT = False
 
 
 # YOLO
-def create_modules(module_defs, img_size, cfg, quantized, quantizer_output, a_bit=8, w_bit=8, FPGA=False, steps=0):
+def create_modules(module_defs, img_size, cfg, quantized, quantizer_output,reorder,TM,TN,a_bit=8, w_bit=8, FPGA=False, steps=0):
     # Constructs module list of layer blocks from module configuration in module_defs
 
     img_size = [img_size] * 2 if isinstance(img_size, int) else img_size  # expand if necessary
@@ -45,7 +45,8 @@ def create_modules(module_defs, img_size, cfg, quantized, quantizer_output, a_bi
                                                                                  bn=bn,
                                                                                  activate=mdef['activation'],
                                                                                  steps=steps,
-                                                                                 quantizer_output=quantizer_output))
+                                                                                 quantizer_output=quantizer_output,
+                                                                                 reorder=reorder,TM=TM,TN=TN))
                 else:
                     modules.add_module('Conv2d', QuantizedConv2d(in_channels=output_filters[-1],
                                                                  out_channels=filters,
@@ -250,7 +251,8 @@ def create_modules(module_defs, img_size, cfg, quantized, quantizer_output, a_bi
                                                                        bn=bn,
                                                                        activate=mdef['activation'],
                                                                        steps=steps,
-                                                                       quantizer_output=quantizer_output))
+                                                                       quantizer_output=quantizer_output,
+                                                                       reorder=reorder,TM=TM,TN=TN))
                 else:
                     modules.add_module('DepthWise2d', QuantizedConv2d(in_channels=output_filters[-1],
                                                                       out_channels=filters,
@@ -617,7 +619,7 @@ class Darknet(nn.Module):
     # YOLOv3 object detection model
 
     def __init__(self, cfg, img_size=(416, 416), verbose=False, quantized=-1, a_bit=8, w_bit=8, FPGA=False,
-                 quantizer_output=False, steps=0):
+                 quantizer_output=False, reorder=False,TM=32,TN=32,steps=0):
         super(Darknet, self).__init__()
 
         if isinstance(cfg, str):
@@ -629,9 +631,14 @@ class Darknet(nn.Module):
         self.w_bit = w_bit
         self.FPGA = FPGA
         self.quantizer_output = quantizer_output  ####输出设置超参数
+        self.reorder = reorder
+        self.TM = TM
+        self.TN = TN
+
         self.hyperparams = copy.deepcopy(self.module_defs[0])
         self.module_list, self.routs = create_modules(self.module_defs, img_size, cfg, quantized=self.quantized,
-                                                      quantizer_output=self.quantizer_output, a_bit=self.a_bit,
+                                                      quantizer_output=self.quantizer_output,reorder=self.reorder,
+                                                      TM=self.TM,TN=self.TN, a_bit=self.a_bit,
                                                       w_bit=self.w_bit, FPGA=self.FPGA, steps=steps)
         self.yolo_layers = get_yolo_layers(self)
         # torch_utils.initialize_weights(self)
